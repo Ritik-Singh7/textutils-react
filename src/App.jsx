610e2a622355581f584bb825145470d854dd1d12
Ritik Singh <ritik9773971093@gmail.com>
2026-06-26 11:47:49 +0530

    Use HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Contant from "./Contant";
 import About from "./About";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
 
       <Routes>
         <Route exact path="/" element={<Contant />} />
         <Route exact path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
